d846f12603edc268c34c9b61e9ac0af12a06fdb3
isabelavieira11 <166832853+isabelavieira11@users.noreply.github.com>
2025-05-18 21:08:25 +0000

    nível novato pronto + comentários dos movimentos das peças

--- xadrez.c
+++ xadrez.c
@@ -8,8 +8,11 @@ int main() {
     int torremov = 5;
     int bispomov = 5;
     int rainhmov = 8;
+    //fim das variáveis de movimentação das peças
 
     // Implementação de Movimentação da Torre
+    // A movimentação da torre é feita em linha reta, tanto na vertical quanto na horizontal.
+    // A torre pode se mover quantas casas quiser, desde que não haja nenhuma peça no caminho.
     int i = 0;
     printf("Torre iniciou o movimento:\n");
     while (i < torremov) 
@@ -18,7 +21,10 @@ int main() {
         i++;
     }
     printf("Torre finalizou o movimento.\n\n");
+
     // Implementação de Movimentação do Bispo
+    // O bispo se move em diagonal, quantas casas quiser, desde que não haja nenhuma peça no caminho.
+    // O bispo pode se mover em qualquer direção diagonal.
     i = 0;
     printf("Bispo iniciou o movimento:\n");
     do
@@ -27,7 +33,10 @@ int main() {
         i++;    /* code */
     } while (i < bispomov);
     printf("Bispo finalizou o movimento.\n\n");
+    
     // Implementação de Movimentação da Rainha
+    // A rainha se move em linha reta, tanto na vertical quanto na horizontal, e também em diagonal.
+    // A rainha pode se mover quantas casas quiser, desde que não haja nenhuma peça no caminho.
     printf("Rainha iniciou o movimento:\n");
     for (i = 0; i < rainhmov; i++)
     {
